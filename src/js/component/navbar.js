d4691ac443db52d95b26e13781eb736ddea21a5b
Jose Hurtado <hello@hurtadojose.com>
2022-07-16 13:41:54 +0000

    NavBar Logo

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import NavLogo from "/src/img/star-wars-logo.png"
 
 export const Navbar = () => {
 	return (
+		<div className="container">
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img className="NavLogo" src={NavLogo} />
+				<span className="navbar-brand mb-0 h1"></span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-warning">Check the Context in action</button>
 				</Link>
 			</div>
 		</nav>
+		</div>
 	);
 };
